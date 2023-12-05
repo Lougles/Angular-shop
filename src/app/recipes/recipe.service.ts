554c6303b08e1eaa2ecccea7a8027532fff4722c
vovacelidze <louglescom@gmail.com>
2023-12-05 22:31:00 +0200

    [Section-10] Add ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingridient.module';
 
 @Injectable({
   providedIn: 'root',
@@ -8,14 +9,22 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is simply test',
-      'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg'
+      'Ratatouille',
+      'A super tasty ratatouille',
+      'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Feirs', 20)
+      ]
     ),
     new Recipe(
-      'Name',
-      'This is another name',
-      'https://static01.nyt.com/images/2023/03/19/multimedia/SS-Oven-Roasted-Chicken-Shawarma-phmt/SS-Oven-Roasted-Chicken-Shawarma-phmt-articleLarge.jpg?w=1280&q=75'
+      'Fried potato',
+      'What else you need to eat?',
+      'https://static01.nyt.com/images/2023/03/19/multimedia/SS-Oven-Roasted-Chicken-Shawarma-phmt/SS-Oven-Roasted-Chicken-Shawarma-phmt-articleLarge.jpg?w=1280&q=75',
+      [
+        new Ingredient('Potato', 10),
+        new Ingredient('Sauces', 2)
+      ]
     ),
   ];
   public getRecipes(): Recipe[] {
